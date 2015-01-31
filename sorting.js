57a0149c3163f604ea55d258c30f00386da1f7d1
Alex Liu <yuningalexliu@gmail.com>
2015-01-31 16:28:45 -0500

    YC 12 power

--- sorting.js
+++ sorting.js
@@ -1,18 +1,97 @@
 function bubbleSort(array) {
 
-    /* your code here */
+	for(var j = 0; j < array.length; j++) {
 
+	    for(var i = 0; i < array.length; i++) {
+	    	var current = array[i];
+	    	var next = array[i+1]
+	    	if (next < current) {
+	    		array[i] = next;
+	    		array[i+1] = current;
+	    	}
+	    }
+
+	}
+    return array;
 }
 
 function split(wholeArray) {
 
     /* your code here to define the firstHalf and secondHalf arrays */
 
-    return [firstHalf, secondHalf];
+   	var splitIndex = Math.floor((wholeArray.length/2));
+   	var left = wholeArray.slice(0, splitIndex);
+   	var right = wholeArray.slice(splitIndex);
+
+   	var returnArray = [];
+	returnArray.push(left);
+	returnArray.push(right);
+
+   	return returnArray;
+}
+
+function merge(leftArray, rightArray) {
+	var resultArray = [];
+	var indexLeft = 0;
+	var indexRight = 0;
+
+  		for (var i = 0; i < (leftArray.length + rightArray.length); i++) {
+  			// debugger;
+  			if (indexLeft === (leftArray.length)) {
+				resultArray = resultArray.concat(rightArray.slice(indexRight));
+				break;
+			} else if (indexRight === (rightArray.length)) {
+				resultArray = resultArray.concat(leftArray.slice(indexLeft));
+				break;
+			} else if (leftArray[indexLeft] < rightArray[indexRight]) {
+				resultArray.push(leftArray[indexLeft]);
+				indexLeft++;
+				console.log("The left index is " + indexLeft);
+			} else {
+				resultArray.push(rightArray[indexRight]);
+				indexRight++;
+				console.log("The right index is " + indexRight);
+			}
+
+		}
+
+	console.log("up a level")
+	return resultArray;
 }
 
 function mergeSort(array) {
 
-    /* your code here */
+    //Where we split
 
+    if (array.length <= 1) {
+    	return array;
+    }
+
+    var leftArray = [];
+    var rightArray = [];
+
+    splitArray = split(array);
+
+    leftArray = splitArray[0];
+    rightArray = splitArray[1];
+
+    leftArray = mergeSort(leftArray);
+    rightArray = mergeSort(rightArray);
+
+    //Where we merge
+
+    array = merge(leftArray, rightArray);
+    return array;
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- sorting.spec.js
+++ sorting.spec.js
@@ -1,17 +1,19 @@
 describe('Bubble Sort', function(){
     it('handles an empty array', function(){
-        expect( bubbleSort([]) ).toEqual( [] );
+        expect( bubbleSort([34,3,5,1,13,21,8]) ).toEqual( [1,3,5,8,13,21,34] );
     });
 });
 
 describe('Merge Sort', function(){
     it('is able to merge two sorted arrays', function(){
-        // test the merging algorithm
+        expect( mergeSort([34,3,5,1,13,21,8]) ).toEqual( [1,3,5,8,13,21,34] );
+        expect( mergeSort([6,5,4,3,2,1]) ).toEqual( [1,2,3,4,5,6] );
     });
 });
 
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
-    // your code here 
+    	expect( split([ 34,3 ])).toEqual([ [34],[3] ])
+    	expect( split([ 34,3,5 ])).toEqual([ [34],[3,5] ])
   });
 });
