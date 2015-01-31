3b5f958b7efe29d81e8c8572430f345b6b078597
Alex Liu <yuningalexliu@gmail.com>
2015-01-31 14:55:02 -0500

    first commit working with rascal randy

---
+++ sorting.js
@@ -0,0 +1,18 @@
+function bubbleSort(array) {
+
+    /* your code here */
+
+}
+
+function split(wholeArray) {
+
+    /* your code here to define the firstHalf and secondHalf arrays */
+
+    return [firstHalf, secondHalf];
+}
+
+function mergeSort(array) {
+
+    /* your code here */
+
+}
---
+++ sorting.spec.js
@@ -0,0 +1,17 @@
+describe('Bubble Sort', function(){
+    it('handles an empty array', function(){
+        expect( bubbleSort([]) ).toEqual( [] );
+    });
+});
+
+describe('Merge Sort', function(){
+    it('is able to merge two sorted arrays', function(){
+        // test the merging algorithm
+    });
+});
+
+describe('Split Array function', function() {
+  it('is able to split an array into two halves', function() {
+    // your code here 
+  });
+});
